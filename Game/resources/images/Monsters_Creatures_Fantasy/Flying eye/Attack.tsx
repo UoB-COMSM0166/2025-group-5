91b8d3e0a644ae46b69342b7c5c9c8177cbe33b3
chyx-17 <949409591@qq.com>
2025-02-10 16:21:48 +0000

    new characters

---
+++ Game/resources/images/Monsters_Creatures_Fantasy/Flying eye/Attack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Attack" tilewidth="16" tileheight="16" tilecount="675" columns="75">
+ <image source="Attack.png" width="1200" height="150"/>
+</tileset>
