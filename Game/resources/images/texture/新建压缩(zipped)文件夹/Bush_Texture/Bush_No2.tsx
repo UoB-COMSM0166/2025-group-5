b540dabbc55671f82df7a7c37c0509e320e4a32d
chyx-17 <949409591@qq.com>
2025-03-04 14:31:07 +0000

    map

---
+++ Game/resources/images/texture/新建压缩(zipped)文件夹/Bush_Texture/Bush_No2.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Bush_No2" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="Bush_No2.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0.727273" y="2.54545">
+    <polygon points="0,0 4.18182,-2.18182 10.3636,-2.36364 13.8182,0.727273 14.9091,3.45455 15.2727,8.54545 11.2727,12.3636 3.63636,12.3636 0.363636,10.5455 -1.09091,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Game/resources/images/texture/新建压缩(zipped)文件夹/Complex_Texture/Cute_Fantasy_Free/Outdoor_Decor_Free.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Outdoor_Decor_Free" tilewidth="16" tileheight="16" tilecount="84" columns="7">
+ <image source="Outdoor_Decor_Free.png" width="112" height="192"/>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.81818" y="14.7273">
+    <polygon points="0,0 -1.63636,-1.81818 -1.63636,-4.72727 -0.181818,-5.81818 0.181818,-9.45455 1.27273,-11.6364 1.27273,-14.1818 3.45455,-14.3636 4.18182,-12 5.09091,-11.2727 4.36364,-9.63636 5.09091,-6.18182 7.27273,-4.90909 7.27273,-1.81818 5.27273,0.181818"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
